44e0f888c512a5474014e3b82f9eed8d0ab7ff1a
NiYeT <69861875+niyetsin@users.noreply.github.com>
2022-06-05 23:23:07 -0400

    Delete lab2 directory

--- lab2/prueba.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-mail = requests.get("https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1")
-mail_split = mail.json()[0].split("@")
-print(mail_split)
-contenido = requests.get("https://www.1secmail.com/api/v1/?action=getMessages&login="+mail_split[0]+"&domain="+mail_split[1])
-print(contenido.json())
